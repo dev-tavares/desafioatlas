34dcf8ebd3544d4e6cb50a1d78fb63c517f50cb2
Daniel Tavares <daniel1tavares@outlook.com>
2022-08-04 21:37:25 -0300

    fix: function getUserInfo import

--- desafio-atlas/src/pages/GlobalUserSearch.jsx
+++ desafio-atlas/src/pages/GlobalUserSearch.jsx
@@ -1,19 +1,24 @@
+import { useHistory } from 'react-router-dom';
 import React, { useState } from 'react';
-import getUserInfo from '../utils/getUserInfo';
+import { getUserInfo } from '../utils/getUserInfo';
 
 function GlobalUserSearch() {
+  const history = useHistory();
   const [userValue, setUserValue] = useState('');
 
   const searchUser = async () => {
     const data = await getUserInfo(userValue);
     console.log(data);
+    history.push(`/${userValue}`);
   };
 
   return (
     <div>
+      <h1>Buscar Repositório no Github</h1>
       <input
         type="text"
         name="userValue"
+        placeholder="digite o nome do usuário"
         onChange={
           ({ target }) => setUserValue(target.value)
         }
